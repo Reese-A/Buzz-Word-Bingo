2cbd48e5a0576dd9336fe9d2069973a1452d74ea
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-24 13:53:17 -1000

    initial commit, server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, (err) => {
+  process.stdout.write(`Server listening on port: ${PORT}\r\n`);
+});
