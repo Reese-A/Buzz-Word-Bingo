6f2e5a652393db6de2debbd3bc6ffc7c1c1cb685
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-24 13:59:18 -1000

    installed express

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 
+app.get(express.static('./public'));
 
 app.listen(PORT, (err) => {
   process.stdout.write(`Server listening on port: ${PORT}\r\n`);
