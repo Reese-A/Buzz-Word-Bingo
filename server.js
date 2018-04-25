9700d1910503a7273841d0863210c19f203c171b
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-24 14:56:28 -1000

    post working, point floatparsing working

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.get('/buzzwords', (req, res) => {
 });
 
 app.post('/buzzword', (req, res)=>{
-
+  console.log(req.body.points)
+  let pointNum = parseFloat(req.body.points);
+  if (typeof(pointNum) !== 'number'){
+    res.send({'success': false})
+  };
+  req.body.points = pointNum;
+  buzzWords.push(req.body);
+  res.send({'success': true})
 })
 
 app.listen(PORT, (err) => {
