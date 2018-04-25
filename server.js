20df9098b08c00723674f6f7a205925be53586f4
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-24 14:42:34 -1000

    installed body-parse setup buzzWords array an get

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
+let buzzWords = [];
 
-app.get(express.static('./public'));
+app.use(express.static('./public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/buzzwords', (req, res) => {
+  res.json({'buzzWords': buzzWords});
+});
+
+app.post('/buzzword', (req, res)=>{
+
+})
 
 app.listen(PORT, (err) => {
   process.stdout.write(`Server listening on port: ${PORT}\r\n`);
